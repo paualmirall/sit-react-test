6a7749e45423da53b7c52e448153b0636dcdac6a
ge <yifei.ge@mail.schwarz>
2025-11-11 11:21:51 +0100

    - Added the ProductCard component - Added basic inline style in the ProductCard component - Removed the body styles in the index.css for avoiding styling issues later when the candidate solve the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ProductCard from './ProductCard.tsx';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
         <h2>Welcome to the</h2>
         <h1>Technical Interview: Product Search (React)</h1>
         <p>Please read the instructions in <b>/INSTRUCTIONS-en.md</b></p>
+        <ProductCard/>
     </>
   )
 }
